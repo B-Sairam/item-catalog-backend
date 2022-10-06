e63eec52c6c3f938b3e1dc6359c696feed9b7953
SAIRAM <87373136+B-Sairam@users.noreply.github.com>
2022-10-06 13:32:52 +0530

    .

--- library/dbConfig.js
+++ library/dbConfig.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async()=>{
     try {
-        const conn = await mongoose.connect('mongodb+srv://sairam77:Sairam2002@sairam.pvjby.mongodb.net/foodDelivery',{
+        const conn = await mongoose.connect(process.env.MONGO_URL,{
             useNewUrlParser:true,
             useUnifiedTopology:true,
         });
